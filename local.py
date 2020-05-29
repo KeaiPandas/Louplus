fbbb7a661b4ea8b84989d022e0c15e3591029d7c
KeaiPandas <zkphao3@gmail.com>
2020-05-29 17:56:24 +0800

    param and math

---
+++ local.py
@@ -0,0 +1,15 @@
+#!/user/bin/env python3
+
+a = 9
+
+def change():
+	global a
+	a = 90
+	print(a)
+
+a = 9
+print("Before the function call",a)
+print("inside change function",end=' ')
+change()
+print("After the function call",a)
+
